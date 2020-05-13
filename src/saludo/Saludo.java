0395547fd4ec58c5839206a9d89b3463a787d01a
jesus <jesus@DESKTOP-DFAPBTV>
2020-05-13 13:00:08 -0600

    ejercicio 1

---
+++ src/saludo/Saludo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package saludo;
+
+/**
+ *
+ * @author jesus
+ */
+public class Saludo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola");
+        System.out.println("hi");
+        System.out.println("hello");
+    }
+    
+}
